d0eb9905477be512710e4664e4a06e325a926b66
Marvin <marvk42@gmail.com>
2018-11-01 21:03:49 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public final class Main {
+    private Main() {
+        throw new AssertionError("No instances of class " + Main.class);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
